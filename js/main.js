cd25595e9d8b2f7262314dd400f7bc3e20ded8d2
Kirubaharan Palani <pkiruba85@gmail.com>
2020-09-09 17:39:48 +0530

    updated image and content based on feedback

--- js/main.js
+++ js/main.js
@@ -98,12 +98,35 @@
         Magnific Popup
     --------------------*/
     $('.video-popup').magnificPopup({
-        type: 'iframe'
+        disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
     });
 
     $('.image-popup').magnificPopup({
         type: 'image'
     });
+
+    $('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
 
     /*------------------
         Magnific Popup
